201e14219634344369895bc55dfc95b407db4167
Jyotsana Khaparde <jyotsanakhaparde0597@gmail.com>
2020-07-28 10:12:55 +0530

    added js sample file

---
+++ sample.js
@@ -0,0 +1,8 @@
+const sample = (inputValue) => {
+    console.log('i am inside the sample function')
+    if(inputValue) {
+        console.log('The input value is -> ', inputValue)
+    }
+}
+
+sample()
