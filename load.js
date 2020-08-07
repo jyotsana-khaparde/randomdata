c921df13eeecf36ec2b3b4112bb8645a6a816455
Jyotsana Khaparde <jyotsanakhaparde0597@gmail.com>
2020-08-07 15:19:26 +0530

    script for wait 15 minutes

--- load.js
+++ load.js
@@ -3,7 +3,7 @@ function sleep(time) {
 }
 
 const load = async () => {
-    for (let i = 1; i < 10000 ; i++) {
+    for (let i = 1; i < 10000; i++) {
         await sleep(900000) //wait for 15 min. (900000)
         console.log("tested load of workflow( "+ i +" )times");;
     }
